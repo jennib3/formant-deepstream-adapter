47dfdb46b49efa9a3316de42f60ca2936bf185bc
jennib3 <briansaccount@gmail.com>
2021-09-14 13:57:20 -0600

    added data

--- deepstream-test3-formant/deepstream_test_3.py
+++ deepstream-test3-formant/deepstream_test_3.py
@@ -384,7 +384,7 @@ def main(args):
         print("Creating H265 Encoder")
     if not encoder:
         sys.stderr.write(" Unable to create encoder")
-    encoder.set_property('bitrate', bitrate)
+    encoder.set_property('bitrate', 5000)
     if is_aarch64():
         encoder.set_property('preset-level', 1)
         encoder.set_property('insert-sps-pps', 1)
